56620fdbe86b71929a08db6f8fd2fd884560d9f1
MatiRosero <gekidasa@gmail.com>
2017-03-16 18:08:23 -0600

    first commit

---
+++ components/footer/site-info.php
@@ -0,0 +1,5 @@
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'centraldesign' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'centraldesign' ), 'WordPress' ); ?></a>
+	<span class="sep"> | </span>
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'centraldesign' ), 'Dentral Design Studios', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+</div><!-- .site-info -->
---
+++ inc/jetpack.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.me/
+ *
+ * @package CentralDesignStudios
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/responsive-videos/
+ */
+function centraldesign_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'	=> 'centraldesign_infinite_scroll_render',
+		'footer'	=> 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Social Menus
+	add_theme_support( 'jetpack-social-menu' );
+
+	// Add theme support for JetPack Portfolio
+	add_theme_support( 'jetpack-portfolio' );
+}
+add_action( 'after_setup_theme', 'centraldesign_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function centraldesign_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'components/post/content', 'search' );
+		else :
+			get_template_part( 'components/post/content', get_post_format() );
+		endif;
+	}
+}
+
+function centraldesign_social_menu() {
+	if ( ! function_exists( 'jetpack_social_menu' ) ) {
+		return;
+	} else {
+		jetpack_social_menu();
+	}
+}
